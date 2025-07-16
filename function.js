3ff5794d15b0ab72a836876de399b3bc589ce8dd
sadekul-ai <sadekul.psl@gmail.com>
2025-07-16 18:37:54 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,113 @@
+
+
+// function texts () {
+//     console.log("My function is working");
+// }
+// texts ()
+
+// function texts ( num1) {
+//     console.log(num1);
+// }
+// texts (10)
+
+// function texts (num1, num2) {
+//     console.log(num1, num2);
+// }
+// texts (10, 5)
+
+// function texts (num1, num2) {
+//     console.log(num1, num2, 'Abir');
+// }
+// texts (10, 5)
+
+// function num1Andnum2 (num1, num2) {
+//     var num1Subtractnum2 = num1 - num2 ;
+//     console.log(num1Subtractnum2);
+// }
+// num1Andnum2 (15, 7)
+
+// function num1Andnum2 (num1, num2) {
+//     var num1Sumnum2 = num1 + num2 ;
+//     return num1Sumnum2 
+// }
+// var num1Sumnum2Result = num1Andnum2 (7, 3)
+// console.log(num1Sumnum2Result);
+
+
+// function num1Andnum2 (num1, num2) {
+//     var num1Subtractnum2 = num1 - num2 ;
+//     return num1Subtractnum2 
+// }
+// var num1Subtractnum2Result = num1Andnum2 (20, 12);
+// console.log(num1Subtractnum2Result);
+
+// function num1Andnum2 (num1, num2) {
+//     var num1Multiplynum2 = num1 * num2 ;
+//     return num1Multiplynum2
+
+// }
+// var num1Multiplynum2Result = num1Andnum2 (3,2)
+// console.log(num1Multiplynum2Result);
+
+// function num1Andnum2 (num1, num2) {
+//     var num1Dividednum2 = num1 / num2 ;
+//     return num1Dividednum2
+// }
+// var num1Dividednum2Result = num1Andnum2 (8, 2)
+// console.log(num1Dividednum2Result);
+
+// var friendDetails = {
+//     name : 'Abir',
+//     age : 14,
+//     class : 9
+// }
+// console.log(friendDetails);
+
+// var friendDetails = {
+//     name : 'Abir',
+//     age : 14,
+//     class : 9,
+//     profession : 'student'
+// }
+// // var name = friendDetails.name;
+// // // console.log(name);
+
+// var friendDetails = {
+//     name : 'Abir',
+//     age : 14,
+//     class : 9,
+//     profession : 'student'
+// }
+// // var whatDoesHe = friendDetails.profession;
+// // // console.log(whatDoesHe);
+
+// var friendDetails = {
+//     name : 'Abir',
+//     age : 14,
+//     class : 9,
+//     profession : 'student'
+// }
+// // friendDetails.name = 'Abir Ahmed'
+// // console.log(friendDetails); // 1st Way
+
+// friendDetails ['age'] = 20,
+// // console.log(friendDetails); // 2nd Way
+
+// var friendDetails = {
+//     name : 'Abir',
+//     age : 14,
+//     class : 9,
+//     profession : 'student'
+// }
+// // friendDetails.name = 'Abir Ahmed'
+// // console.log(friendDetails.name); 
+
+
+
+
+
+
+
+
+
+
